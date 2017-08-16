8e9ee8a7fb27507551b31faec34d4314354dd85a
anggraito <anggraito.laras@gmail.com>
2017-08-16 17:53:13 +0700

    update user route

--- routers/user.js
+++ routers/user.js
@@ -4,10 +4,62 @@ var router = express.Router()
 var models =  require('../models')
 
 router.get('/', (req, res)=>{
-  res.render('user')
+  models.User.findAll()
+  .then((users)=>{
+      res.render('user', {data_users: users})
+  })
+  .catch(err=>{
+    res.send(err)
+  })
 })
 
 router.get('/add', (req, res)=>{
   res.render('addUser')
+})
+
+router.post('/add', (req, res)=>{
+  models.User.create(req.body)
+  .then(()=>{
+    res.redirect('/user')
+  })
+  .catch(err=>{
+    res.send(err)
+  })
+})
+
+router.get('/edit/:id', (req, res)=>{
+  models.User.findById(req.params.id)
+  .then((user)=>{
+    res.render('editUser', {edit_user: user})
+  })
+  .catch(err=>{
+    res.send(err)
+  })
+})
+
+router.post('/edit/:id', (req, res)=>{
+  models.User.update({
+    name: req.body.name,
+    email: req.body.email,
+    telp: req.body.phone,
+    address: req.body.address
+  },{
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(()=>{
+    res.redirect('/user')
+  })
+  .catch(err=>{
+    res.send(err)
+  })
+})
+
+router.get('/delete/:id', (req, res)=>{
+  models.User.destroy({where:{id: req.params.id}})
+  .then(()=>{
+    res.redirect('/user')
+  })
 })
 module.exports = router
